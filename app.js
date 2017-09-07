05586586d9ec6756f6d42362734d608f6be69ea6
toniau <toni.au94@gmail.com>
2017-09-06 20:00:20 -0400

    Added Kelvin conversion

--- app.js
+++ app.js
@@ -5,9 +5,29 @@ convert.cToF = function(celsius) {
   return celsius * 9 / 5 + 32;
 }
 
+convert.cToK = function(celsius) {
+	if(!Number.isInteger(celsius)) return undefined;
+	return celsius + 273.15;
+}
+
 convert.fToC = function(fahrenheit) {
   if(!Number.isInteger(fahrenheit)) return undefined;
   return (fahrenheit - 32) * 5 / 9;
+}
+
+convert.fToK = function(fahrenheit) {
+	if(!Number.isInteger(fahrenheit)) return undefined;
+	return (fahrenheit + 459.67) * 5 / 9;
+}
+
+convert.kToC = function(kelvin) {
+	if(!Number.isInteger(kelvin)) return undefined;
+	return kelvin - 273.15;
+}
+
+convert.kToF = function(kelvin) {
+	if(!Number.isInteger(kelvin)) return undefined;
+	return kelvin - 459.67;
 }
 
 module.exports = convert;
--- test/test.js
+++ test/test.js
@@ -2,23 +2,29 @@ let convert = require('../app.js');
 let assert = require('assert');
 
 describe('Temperature Conversion', function(){
-	describe('cToF',function() {
+	describe('cToF/K',function() {
 		it('should convert -40C to -40F', function(){
 			assert.equal(-40, convert.cToF(-40));
 		});
 		it('should convert 0C to 32F', function(){
 			assert.equal(32, convert.cToF(0));
 		});
+		it('should convert 0C to 273.15K', function(){
+			assert.equal(273.15, convert.cToK(0));
+		});
 		it('should return undefined if no temperature input', function(){
 			assert.equal(undefined, convert.cToF(''));
 		});
 	});
-	describe('fToC', function(){
+	describe('fToC/K', function(){
 		it('should convert -40F to -40C', function(){
 			assert.equal(-40, convert.fToC(-40));
 		});
 		it('should convert 32F to 0C', function(){
 			assert.equal(0, convert.fToC(32));
+		});
+		it('should convert 32F to 273.15K', function(){
+			assert.equal(273.15, convert.fToK(32));
 		});
 		it('should return undefined if no temperature input', function(){
 			assert.equal(undefined, convert.fToC(''));
